7f95ec6f64a512eb8a02b3e123de039a7481dd96
math09 <hernandezmathis@gmail.com>
2023-06-01 10:38:09 +0200

    new interface

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="el"></div>
+<div class="row">
+  <div class="col-lg-1"></div>
+  <div class="col-lg-10 fully">
+    <h1>coucou</h1>
+    <br>
+    <h1>coucou</h1>
+    <br>
+    <h1>coucou</h1>
+    <br>
+    <h1>coucou</h1>
+    <br>
+  </div>
+  <div class="col-lg-1"></div>
+</div>
 </template>
 
 <script>
@@ -35,5 +49,9 @@ body{
 
   background: linear-gradient(to bottom, rgba(15, 63, 187, 0.79), rgba(52, 150, 235, 0.79));
   border-radius: 50%;
+}
+.fully{
+  background: rgb(65, 51, 51);
+  z-index: 0;
 }
 </style>
