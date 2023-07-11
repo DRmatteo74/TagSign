bdd1f17dac974c79d8327b0044d02437e3eaeed5
math09 <hernandezmathis@gmail.com>
2023-07-12 01:18:58 +0200

    APTab added

---
+++ src/components/APListEleves.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <q-table
+    flat
+      title="Éleves"
+      :rows="rows"
+      :columns="columns"
+      row-key="nom"
+      card-class="bg-galette"
+    />
+  </div>
+</template>
+
+<script>
+const columns = [
+  { name: 'nom', required: true, label: 'Nom', align: 'center', field: row => row.nom, sortable: true },
+  { name: 'Prenom', align: 'center', label: 'Prenom', field: row => row.prenom, sortable: true },
+]
+
+const rows = [
+  {
+    nom: 'DUVAL',
+    prenom: 'Vincent',
+    date: '04//07/2023',
+    cours: 'UML',
+    intervenant: 'BONCHE',
+    justificatif: null,
+  },
+  {
+    nom: 'JEAN',
+    prenom: 'Anthony',
+    date: '04//07/2023',
+    cours: 'Assembleur',
+    intervenant: 'BOUMGHAR',
+    justificatif: null,
+  },
+  {
+    nom: 'PEYRARD',
+    prenom: 'Tibaut',
+    date: '04//07/2023',
+    cours: 'java',
+    intervenant: 'JESEPLUS',
+    justificatif: null,
+  },
+  {
+    nom: 'DI-RIENZO',
+    prenom: 'Matteo',
+    date: '04//07/2023',
+    cours: 'Droit',
+    intervenant: 'CLOPPET',
+    justificatif: null,
+  },
+  {
+    nom: 'HERNANDEZ',
+    prenom: 'Mathis',
+    date: '04//07/2023',
+    cours: 'Application Androîd',
+    intervenant: 'VARDAGNANT',
+    justificatif: null,
+  },
+]
+
+export default {
+  setup () {
+    return {
+      columns,
+      rows
+    }
+  }
+}
+</script>
+<style>
+.bg-galette {
+  background: #ffffff !important;
+  border-radius: 10px;
+}
+</style>
---
+++ src/pages/APTab.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="card">
+      <q-tabs
+        v-model="tab"
+        dense
+        class="bg-moduleBackgroundColor"
+        active-color="primary"
+        indicator-color="primary"
+        align="justify"
+        narrow-indicator
+      >
+        <q-tab name="absences" label="absences" 
+        @click="changeMenu" v-bind:disable="isDistanciel"/>
+        <q-tab name="eleves" label="élèves" 
+        @click="changeMenu"/>
+        <q-tab name="planning" label="planning" 
+        @click="changeMenu"/>
+      </q-tabs>
+      <q-separator />
+  
+      <q-tab-panels v-model="tab" animated class="bg-moduleBackgroundColor" style="height: 95%;">
+        <q-tab-panel name="absences">
+          <Absences/>
+        </q-tab-panel>
+  
+        <q-tab-panel v-model="tab" name="eleves">
+        <Eleves/>
+        </q-tab-panel>
+        <q-tab-panel name="planning">
+          <Planning/>
+        </q-tab-panel>
+      </q-tab-panels>
+    </div>
+  </template>
+  
+<script>
+import Planning from '@/components/planning.vue'
+import Absences from '@/components/APStudentTable.vue'
+import Eleves from '@/components/APListEleves.vue'
+
+  export default {
+    components: {
+  Planning,
+  Absences,
+  Eleves,
+},
+    data() {
+      return {
+        tab: 'absences',
+      };
+    },
+};
+</script>
+  
+<style>
+    .card {
+        background: var(--cardColor);
+        width: 100%;
+        height: 100%;
+        padding: 30px;
+        border-radius: 15px;
+    }
+
+    .random_container {
+        height: 100%;
+    }
+
+    .random_element{
+        padding: 5px 10px 5px 10px;
+        font-size: medium;
+        font-weight: bolder;
+        border-radius: 50px;
+    }
+</style>
+  
