4ac4c5e6e119d0c447b00328ba8f5df320b879df
math09 <hernandezmathis@gmail.com>
2023-07-13 14:57:49 +0200

    good home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,91 +1,91 @@
 <template>
-    <div class="card">
-        <div class="q-pa-md example-row-equal-width fit">
-            <div class="row justify-between fit">
-                <div class="column q-mr-md" style="width: 35%; height: 100%;">
-                    <div class="col-auto q-mb-md">
-                        <NextClass :is-prof="isProf" v-if="(isEleve || isProf) && !isAp"/>
-                        <APEcoles v-if="isAp" @ecole-selected="handleEcoleSelected"/>
-                    </div>
-                    <div class="col" >
-                        <Absence v-if="isEleve"/>
-                        <APClasses v-if="isAp" :selected-ecole="selectedEcole" @classe-selected="handleClasseSelected"/>
-                    </div>
-                </div>
-                <div class="col">
-                    <PlanningSmall  v-if="isEleve || isProf"/>
-                    <APTab v-if="isAp" :selected-ecole="selectedEcole" :selected-classe="selectedClasse"/>
-                </div>
-            </div>
-        </div>
-    </div>
-    <router-view></router-view>
+  <div class="card">
+      <div class="q-pa-md example-row-equal-width fit">
+          <div class="row justify-between fit">
+              <div class="column q-mr-md" style="width: 35%; height: 100%;">
+                  <div class="col-auto q-mb-md">
+                      <NextClass :is-prof="isProf" v-if="(isEleve || isProf) && !isAp"/>
+                      <APEcoles v-if="isAp" @ecole-selected="handleEcoleSelected"/>
+                  </div>
+                  <div class="col" >
+                      <Absence v-if="isEleve"/>
+                      <APClasses v-if="isAp" :selected-ecole="selectedEcole" @classe-selected="handleClasseSelected"/>
+                  </div>
+              </div>
+              <div class="col">
+                  <PlanningSmall  v-if="isEleve || isProf"/>
+                  <APTab v-if="isAp" :selected-ecole="selectedEcole" :selected-classe="selectedClasse"/>
+              </div>
+          </div>
+      </div>
+  </div>
+  <router-view></router-view>
 </template>
 
 <script>
-    import { ref } from 'vue'
-    import NextClass from '@/components/NextClass.vue'
-    import PlanningSmall from '@/components/Planning.vue'
-    import Absence from '@/components/AbsenceEleveModule.vue'
-    import APClasses from '@/components/APClasses.vue'
-    import APEcoles from '@/components/APEcoles.vue'
-    import APTab from './APTab.vue'
+  import { ref } from 'vue'
+  import NextClass from '@/components/NextClass.vue'
+  import PlanningSmall from '@/components/Planning.vue'
+  import Absence from '@/components/AbsenceEleveModule.vue'
+  import APClasses from '@/components/APClasses.vue'
+  import APEcoles from '@/components/APEcoles.vue'
+  import APTab from './APTab.vue'
 
-    export default {
-        name: 'HomePage',
-        
-        components: {
-            NextClass,
-            PlanningSmall,
-            Absence,
-            APClasses,
-            APEcoles,
-            APTab
-        },
+  export default {
+      name: 'HomePage',
+      
+      components: {
+          NextClass,
+          PlanningSmall,
+          Absence,
+          APClasses,
+          APEcoles,
+          APTab
+      },
 
-        setup () {
-            return {
-                leftDrawerOpen: ref(false)
-            }
-        },
+      setup () {
+          return {
+              leftDrawerOpen: ref(false)
+          }
+      },
 
-        data(){
-            return {
-                isProf : false,
-                isEleve : true,
-                isAp : false,
-                isAdmin : false,
-                selectedEcole: null,
-                selectedClasse: null
-            }
-        },
-        mounted() {
-            const roles = JSON.parse(localStorage.getItem('roles'));
+      data(){
+          return {
+              isProf : false,
+              isEleve : true,
+              isAp : false,
+              isAdmin : false,
+              selectedEcole: null,
+              selectedClasse: null
+          }
+      },
+      mounted() {
+          const roles = JSON.parse(localStorage.getItem('roles'));
 
-            if (roles && roles.length > 0) {
-                this.isProf = roles.includes('ROLE_PROF');
-                this.isEleve = roles.includes('ROLE_ELEVE');
-                this.isAp = roles.includes('ROLE_AP');
-                this.isAdmin = roles.includes('ROLE_ADMIN');
-            }
-        },
-        methods: {
-            handleEcoleSelected(selectedEcole) {
-                this.selectedEcole = selectedEcole;
-            },
-            handleClasseSelected(selectedClasse){
-                this.selectedClasse = selectedClasse;
-            }
-        }
-    }
+          if (roles && roles.length > 0) {
+              this.isProf = roles.includes('ROLE_PROF');
+              this.isEleve = roles.includes('ROLE_ELEVE');
+              this.isAp = roles.includes('ROLE_AP');
+              this.isAdmin = roles.includes('ROLE_ADMIN');
+          }
+      },
+      methods: {
+          handleEcoleSelected(selectedEcole) {
+              this.selectedEcole = selectedEcole;
+          },
+          handleClasseSelected(selectedClasse){
+              this.selectedClasse = selectedClasse;
+          }
+      }
+  }
 </script>
 
 <style>
-    .card{
-      background: var(--cardColor);
-      width: 100%;
-      height: 100%;
-      padding: 30px;
-      border-radius: 15px;
-    }
+  .card{
+    background: var(--cardColor);
+    width: 100%;
+    height: 100%;
+    padding: 30px;
+    border-radius: 15px;
+  }
 </style>
